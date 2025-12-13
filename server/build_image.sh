3f5d7eba7e0a3c534552fbef02ec65ff23578ada
Suvanjan Mukherjee <suvanjanmukherjee@gmail.com>
2025-12-13 14:35:59 -0500

    Remove Graalvm native image

--- server/build_image.sh
+++ server/build_image.sh
@@ -2,4 +2,4 @@
 
 set -eu
 
-./mvnw -Pnative spring-boot:build-image -Dspring-boot.build-image.imageName=ghcr.io/smukherj1/masonry/server:latest
+./mvnw spring-boot:build-image -Dspring-boot.build-image.imageName=ghcr.io/smukherj1/masonry/server:latest
