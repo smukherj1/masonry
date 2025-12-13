c9a1d06c2cfaacd41ddfcebc6ed9927614dc0dfe
Suvanjan Mukherjee <suvanjanmukherjee@gmail.com>
2025-12-13 14:33:46 -0500

    Implement blob upload logic

--- server/src/main/kotlin/com/github/smukherj1/masonry/server/controllers/BlobsServiceController.kt
+++ server/src/main/kotlin/com/github/smukherj1/masonry/server/controllers/BlobsServiceController.kt
@@ -1,11 +1,12 @@
 package com.github.smukherj1.masonry.server.controllers
 
 import com.github.smukherj1.masonry.server.errors.safeRun
-import com.github.smukherj1.masonry.server.proto.BeginUploadRequest
-import com.github.smukherj1.masonry.server.proto.BeginUploadResponse
+import com.github.smukherj1.masonry.server.models.UploadStatus
 import com.github.smukherj1.masonry.server.proto.BlobsServiceGrpc
 import com.github.smukherj1.masonry.server.proto.CompleteUploadRequest
 import com.github.smukherj1.masonry.server.proto.CompleteUploadResponse
+import com.github.smukherj1.masonry.server.proto.GetUploadRequest
+import com.github.smukherj1.masonry.server.proto.GetUploadResponse
 import com.github.smukherj1.masonry.server.proto.UploadRequest
 import com.github.smukherj1.masonry.server.proto.UploadResponse
 import com.github.smukherj1.masonry.server.services.BlobUploadService
@@ -13,6 +14,7 @@ import io.grpc.Status
 import io.grpc.stub.StreamObserver
 import org.slf4j.LoggerFactory
 import org.springframework.grpc.server.service.GrpcService
+import java.util.UUID
 
 @GrpcService
 class BlobsServiceController(
@@ -21,38 +23,28 @@ class BlobsServiceController(
 
     val log = LoggerFactory.getLogger(BlobsServiceController::class.java)
 
-    override fun beginUpload(request: BeginUploadRequest?, responseObserver: StreamObserver<BeginUploadResponse?>?) {
-        log.info("beginUpload(): uploadId=${request?.uploadId}")
-        if(responseObserver == null) return
-        if(request == null) {
-            responseObserver.onError(Status.INVALID_ARGUMENT.withDescription("request was null").asRuntimeException())
-            return
-        }
-
-        responseObserver.safeRun {
-            blobUploadService.beginUpload(request.uploadId)
-            responseObserver.onNext(BeginUploadResponse.newBuilder().setUploadId(request.uploadId).build())
-            responseObserver.onCompleted()
-        }
-    }
-
     override fun upload(responseObserver: StreamObserver<UploadResponse?>?): StreamObserver<UploadRequest?>? {
         if (responseObserver == null) return null
         return object : StreamObserver<UploadRequest?>{
             var uploadId: String? = null
+            var serverUploadId: String? = null
 
             override fun onNext(u: UploadRequest?) {
                 if(u == null) return
                 if(u.uploadId.isNullOrBlank()) return
                 if(uploadId == null) {
                     uploadId = u.uploadId
+                    serverUploadId = UUID.randomUUID().toString()
                 } else if (!uploadId.equals(u.uploadId)) {
                     responseObserver.onError(Status.INVALID_ARGUMENT.withDescription("uploadId can't be changed mid-stream, got initial id $uploadId but current id is ${u.uploadId}").asRuntimeException())
                     return
+                } else if(serverUploadId.isNullOrBlank()) {
+                    responseObserver.onError(Status.INTERNAL.withDescription("internal upload identifier was not generated for $uploadId").asRuntimeException())
+                    return
                 }
-                log.info("upload(): uploadId=${u.uploadId}, offset=${u.offset}, ${u.data.size()} bytes of data.")
+                log.info("upload(): uploadId=${u.uploadId}, serverUploadId=${serverUploadId}, offset=${u.offset}, ${u.data.size()} bytes of data.")
                 responseObserver.safeRun {
-                    blobUploadService.upload(uploadId = u.uploadId, offset = u.offset, data = u.data.toByteArray())
+                    blobUploadService.upload(uploadId = u.uploadId, serverUploadId = serverUploadId.orEmpty(), offset = u.offset, data = u.data.toByteArray())
                 }
             }
 
@@ -67,7 +59,34 @@ class BlobsServiceController(
                 responseObserver.onError(t)
             }
 
-        };
+        }
+    }
+
+    override fun getUpload(request: GetUploadRequest?, responseObserver: StreamObserver<GetUploadResponse?>?) {
+        if (responseObserver == null) return
+        responseObserver.safeRun {
+            require(request != null) { "request must not be null" }
+            require(request.uploadId.isNullOrBlank()) { "uploadId must not be null or empty" }
+
+            val um = blobUploadService.getUpload(request.uploadId)
+
+            if(um == null) {
+                responseObserver.onError(Status.NOT_FOUND.withDescription("upload ID ${request.uploadId} does not exist").asRuntimeException())
+                return@safeRun
+            }
+            responseObserver.onNext(
+                GetUploadResponse.newBuilder()
+                .setUploadId(um.uploadId)
+                    .setNextOffset(um.nextOffset)
+                    .setUploadStatus(when(um.uploadStatus) {
+                        UploadStatus.UNKNOWN -> com.github.smukherj1.masonry.server.proto.UploadStatus.UPLOAD_STATUS_UNSPECIFIED
+                        UploadStatus.ACTIVE -> com.github.smukherj1.masonry.server.proto.UploadStatus.UPLOAD_STATUS_ACTIVE
+                        UploadStatus.COMPLETED -> com.github.smukherj1.masonry.server.proto.UploadStatus.UPLOAD_STATUS_COMPLETED
+                        UploadStatus.FAILED -> com.github.smukherj1.masonry.server.proto.UploadStatus.UPLOAD_STATUS_FAILED
+                    })
+                .build())
+            responseObserver.onCompleted()
+        }
     }
 
     override fun completeUpload(
--- server/src/main/kotlin/com/github/smukherj1/masonry/server/services/BlobUploadService.kt
+++ server/src/main/kotlin/com/github/smukherj1/masonry/server/services/BlobUploadService.kt
@@ -1,34 +1,90 @@
 package com.github.smukherj1.masonry.server.services
 
 import com.github.smukherj1.masonry.server.models.BlobUploadModel
+import com.github.smukherj1.masonry.server.models.UploadStatus
 import com.github.smukherj1.masonry.server.repositories.BlobRepository
 import com.github.smukherj1.masonry.server.repositories.BlobUploadRepository
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
+import java.io.IOException
+import java.io.RandomAccessFile
 import java.time.LocalDateTime
 import java.time.ZoneOffset
+import kotlin.jvm.optionals.getOrNull
 
 @Service
 class BlobUploadService(
     val blobUploadRepository: BlobUploadRepository,
     val blobRepository: BlobRepository
 ) {
+    val log = LoggerFactory.getLogger(BlobUploadService::class.java)
+    fun upload(uploadId: String, serverUploadId: String, offset: Long, data: ByteArray) {
+        val uploadModel = getOrCreateUpload(uploadId = uploadId, serverUploadId = serverUploadId)
+        require(uploadModel.uploadStatus == UploadStatus.ACTIVE, {"unable to upload with id $uploadId because its state is currently ${uploadModel.uploadStatus}"})
+        require(offset == uploadModel.nextOffset, {"given offset $offset does not match currently expected next offset ${uploadModel.nextOffset}"})
 
-    @Transactional
-    fun beginUpload(uploadId: String) {
-        require(uploadId.isNotBlank()) { "upload id must not be blank" }
-        check(!blobUploadRepository.existsById(uploadId)) { "Upload ID '${uploadId}' already exists" }
-        val now = LocalDateTime.now(ZoneOffset.UTC)
-        blobUploadRepository.save(BlobUploadModel(
-            uploadId = uploadId,
-            createTime = now,
+        try {
+            RandomAccessFile(uploadModel.location, "rw").use { file ->
+                if (file.length() < uploadModel.nextOffset) {
+                    // The upload model should only be updated after we've successfully committed data to the file.
+                    // Thus, the file length being lower than the next expected offset means bytes disappeared or
+                    // some other kind of corruption. Abandon the upload.
+                    blobUploadRepository.save(
+                        uploadModel.copy(
+                            updateTime = localDateTimeNow(),
+                            uploadStatus = UploadStatus.FAILED
+                        )
+                    )
+                    throw RuntimeException("internal committed data offset for upload $uploadId did not match the expected offset ${uploadModel.nextOffset}, upload $uploadId is possibly corrupted and is being marked as failed")
+                }
+                file.seek(uploadModel.nextOffset)
+                file.write(data)
+            }
+        } catch (e: IOException) {
+            log.error("Error uploading ${data.size} bytes of data for upload $uploadId, server ID $serverUploadId, offset $offset", e)
+            throw RuntimeException("error writing ${data.size} bytes of data for upload $uploadId, offset $offset")
+        }
+        val now = localDateTimeNow()
+        blobUploadRepository.save(uploadModel.copy(
             updateTime = now,
-            nextOffset = 0L,
-            location = uploadPath(uploadId)
+            nextOffset = offset + data.size,
         ))
     }
 
-    fun upload(uploadId: String, offset: Long, data: ByteArray) {}
+    fun getUpload(uploadId: String): BlobUploadModel?  = blobUploadRepository.findById(uploadId).getOrNull()
+
+    @Transactional
+    private fun getOrCreateUpload(uploadId: String, serverUploadId: String): BlobUploadModel {
+        require(uploadId.isNotBlank()) { "upload id must not be blank" }
+        require(serverUploadId.isNotBlank()) { "server upload id must not be blank" }
+        val cur = blobUploadRepository.findById(uploadId).getOrNull()
+        val now = localDateTimeNow()
+
+        if(cur == null) {
+            return blobUploadRepository.save(BlobUploadModel(
+                uploadId = uploadId,
+                serverUploadId = serverUploadId,
+                createTime = now,
+                updateTime = now,
+                nextOffset = 0L,
+                location = uploadPath(uploadId),
+                uploadStatus = UploadStatus.ACTIVE,
+            ))
+        }
+        if (!cur.serverUploadId.equals(serverUploadId)) {
+            blobUploadRepository.save(cur.copy(
+                updateTime = now,
+                uploadStatus = UploadStatus.FAILED
+            ))
+            throw IllegalStateException("Possible concurrent uploads detected for upload ID $uploadId, upload is now considered corrupted and a new upload with a different ID must be attempted")
+        }
+        return blobUploadRepository.save(cur.copy(
+            updateTime = now,
+        ))
+    }
 }
 
 private fun uploadPath(uploadId: String) = "uploads/${uploadId}"
+
+private fun localDateTimeNow() = LocalDateTime.now(ZoneOffset.UTC)
